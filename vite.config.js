f5a08acf094f1e1354b784640b76c4f28eac1aae
Daniyal Sohail <daniyalsohail47@gmail.com>
2024-11-11 18:55:17 +0500

    Remove scroll

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['aos', 'swiper/react', 'swiper/css', 'react-scroll', 'swiper/modules'], // Added swiper/css here
-    }
+      external: ['aos', 'swiper/react', 'swiper/css', 'swiper/modules'], // Keep swiper and aos external
+    },
   },
   optimizeDeps: {
-    include: ['react-scroll', 'swiper/css'], // Added swiper/css here for optimization
-  }
-})
+    include: ['swiper/css'], // Only include swiper for optimization
+  },
+});
